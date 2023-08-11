dedc2522a9dc9fc8bb97d9265bef3e9349fd8373
Николай Васильев <nikolay.vasilyev@uksever-invest.ru>
2023-08-11 21:25:16 +0300

    reduce rateLimit

--- middlewares/rateLimit.js
+++ middlewares/rateLimit.js
@@ -1,8 +1,8 @@
 // documentation: https://www.npmjs.com/package/express-rate-limit
 const rateLimit = require('express-rate-limit');
-// Limit each IP to 100 requests per `window` (here, per 10 minutes)
+// Limit each IP to 100 requests per `window` (here, per 1 minute)
 const limiter = rateLimit({
-  windowMs: 10 * 60 * 1000,
+  windowMs: 60 * 1000,
   max: 100,
 });
 
